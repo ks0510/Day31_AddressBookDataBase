c024be814e8c64f8ba8a816e64285dac037d3559
ks0510 <kshaikh051999@gmail.com>
2023-05-25 16:49:54 +0530

    Added sql file to count data size by city and state in data table

---
+++ src/CountSize.sql
@@ -0,0 +1,15 @@
+mysql> select COUNT(*) FROM addressbook_db WHERE city='solapur';
++----------+
+| COUNT(*) |
++----------+
+|        3 |
++----------+
+1 row in set (0.02 sec)
+
+mysql>  select COUNT(*) FROM addressbook_db WHERE state='maharashtra';
++----------+
+| COUNT(*) |
++----------+
+|        6 |
++----------+
+1 row in set (0.00 sec)
